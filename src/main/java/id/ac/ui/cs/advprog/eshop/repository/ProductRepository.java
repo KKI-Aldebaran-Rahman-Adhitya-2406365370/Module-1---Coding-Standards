bbea89414e0b793717e560dbef1c3962554c77b2
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-02-10 14:45:51 +0700

    Implemented delete-product feature with UUID

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,25 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public void delete(String id) {
+        productData.removeIf(product -> product.getProductId().equals(id));
     }
 }
