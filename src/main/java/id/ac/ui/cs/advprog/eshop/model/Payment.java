0c441fdcf7f7a85426c36bc7a9ba7e736b1927fb
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-03-10 14:18:30 +0700

    [GREEN] Implemented payment model to pass unit tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,7 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.util.Map;
 
+@Getter
 public class Payment {
     private String id;
     private String method;
@@ -10,22 +14,35 @@ public class Payment {
     private Order order;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        if (order == null || paymentData == null) {
+            throw new IllegalArgumentException();
+        }
 
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+
+        if ("VOUCHER_CODE".equals(method)) {
+            String voucher = paymentData.get("voucherCode");
+            if (voucher != null && voucher.length() == 16 && voucher.startsWith("ESHOP")) {
+                int numCount = 0;
+                for (char c : voucher.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        numCount++;
+                    }
+                }
+                if (numCount == 8) {
+                    this.status = "SUCCESS";
+                } else {
+                    this.status = "REJECTED";
+                }
+            } else {
+                this.status = "REJECTED";
+            }
+        } else {
+            this.status = "REJECTED";
+        }
     }
 
-    public String getId() {
-        return null;
-    }
-    public String getMethod() {
-        return null;
-    }
-    public String getStatus() {
-        return null;
-    }
-    public Map<String, String> getPaymentData() {
-        return null;
-    }
-    public Order getOrder() {
-        return null;
-    }
 }
