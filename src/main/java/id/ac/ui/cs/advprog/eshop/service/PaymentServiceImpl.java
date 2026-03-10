7bdfdcdd60e3c3d7a543e9b564404c78c8ee42f1
Aldebaran64 <aldebaran26adhitya@gmail.com>
2026-03-10 16:32:44 +0700

    [GREEN] Implemented payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -17,21 +17,30 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, paymentData, order);
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        payment.setStatus(status);
+
+        if (status.equals("SUCCESS")) {
+            payment.getOrder().setStatus("SUCCESS");
+        } else if (status.equals("REJECTED")) {
+            payment.getOrder().setStatus("FAILED");
+        }
+
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayments();
     }
 }
